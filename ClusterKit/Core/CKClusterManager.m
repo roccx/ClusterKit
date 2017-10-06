538b0c585f7de912b2b824f8543e2633f024d1b2
Murat Yilmaz <murat.yilmaz@jamitlabs.com>
2017-10-06 21:14:30 +0200

    Add missing semicolon

--- ClusterKit/Core/CKClusterManager.m
+++ ClusterKit/Core/CKClusterManager.m
@@ -242,7 +242,7 @@ BOOL CLLocationCoordinateEqual(CLLocationCoordinate2D coordinate1, CLLocationCoo
     for (CKCluster *cluster in toRemove) {
         [self.map moveCluster:cluster from:cluster.coordinate to:cluster.coordinate completion:^(BOOL finished) {
             [self.map removeCluster:cluster];
-        }]
+        }];
     }
     
     _clusters = clusters.mutableCopy;
